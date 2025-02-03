f27028728681c3b3aef83c276909fc2d916355ed
Maksim_Shramko <maxramko@gmail.com>
2025-02-03 12:09:28 +0300

    It's a bit hard to rememeber how to create all these code thingies... However, as russians say: It is time to RAZDUPLYAT'SA. Soooo, in this commit there is only a simple code, VERY simple code. And still it's not working right, lol.

--- shramko.maksim/K1/main.cpp
+++ shramko.maksim/K1/main.cpp
@@ -1,2 +1,33 @@
+#include <iostream>
+
+struct BiList
+{
+  int value;
+  BiList * prev, * next;
+};
+
 int main()
-{}
+{
+  int* l = nullptr;
+  try
+  {
+    l = new int[10];
+  }
+  catch (const std::bad_alloc & e)
+  {
+    delete[] l;
+    std::cerr << "Mem err!\n";
+    return 1;
+  }
+
+  int n = 0;
+  for (size_t i = 0; i < 10; i++)
+  {
+    std::cin >> n;
+  }
+
+  for (size_t j = 0; j < 10; j++)
+  {
+    std::cout << l[j];
+  }
+}
