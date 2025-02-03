7a2d439ee6e954e18cb64f9ef27cb0403247c135
A1exCRE <alsmmm2065@gmail.com>
2025-02-03 12:07:54 +0300

    1

--- smirnov.alexandr/K1/main.cpp
+++ smirnov.alexandr/K1/main.cpp
@@ -1,2 +1,37 @@
+#include <iostream>
+
+struct BiList {
+  int value;
+  BiList * prev, * next;
+};
+
+BiList * arrayToBiList(int * numbers, int count)
+{
+  if (count == 0)
+  {
+    return nullptr;
+  }
+  BiList * head = new BiList{numbers[0], nullptr, nullptr};
+  BiList * current = head;
+  for (int i = 1; i < count; ++i)
+  {
+    current->next = new BiList{array[i], current, nullptr};
+    current = current->next;
+  }
+   return head;
+}
+
+
 int main()
-{}
+{
+  constexpr size_t max_size = 10;
+  int * numbers = new int[max_size];
+  size_t count = 0;
+  while (count < max_size || !std::cin.eof())
+  {
+    std::cin >> numbers[count];
+    ++count;
+  }
+  BiList * list = arrayToBiList(numbers, count);
+  delete numbers[];
+}
