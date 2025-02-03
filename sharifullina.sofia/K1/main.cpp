cb024d3af82faeede78428547c92e64b27fcaa11
sharksonk <sharksonk@gmail.com>
2025-02-03 12:14:54 +0300

    spat

--- sharifullina.sofia/K1/main.cpp
+++ sharifullina.sofia/K1/main.cpp
@@ -1,2 +1,49 @@
+#include <iostream>
+
+struct BiList
+{
+  int value;
+  BiList * prev, * next;
+};
+
+Bilist * createList(int * array, size_t size)
+{
+  Bilist * head = new Bilist{array[0], nullptr, nullptr};
+  Bilist * current = head;
+  for (size_t i = 1; i < size; i++)
+  {
+    Bilist * subhead = new Bilist{array[i], current, nullptr};
+    current->next = subhead;
+    current = subhead;
+  }
+  return current;
+}
+
+void deleteList(Bilist * subhead)
+{
+  while (node)
+  {
+    Bilist * prev = node->prev;
+    delete node;
+    node = prev
+  }
+}
+
 int main()
-{}
+{
+  int * array = new int[10];
+  size_t k = 0;
+  while (k < 10 && std::cin >> array[k])
+  {
+    ++k;
+  }
+  Bilist * tail = createList(array, k);
+  delete[] array;
+  for (BiList* node = tail; node; node = node->prev)
+  {
+    std::cout << node->value << " ";
+  }
+  deleteBiList(tail);
+  return 0;
+}
+
