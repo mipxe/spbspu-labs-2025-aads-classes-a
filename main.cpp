9284e3789d195cef5cfdb09bd6cda6c2f0759a76
“BalashovDima” <zoozoz98765@gmail.com>
2025-02-03 21:54:49 +0300

    garry potter update

--- main.cpp
+++ main.cpp
@@ -2,54 +2,93 @@
 
 struct List
 {
-	int value;
-	List * next;
-	List * before;
+  int value;
+  List * next;
+  List * before;
 };
+
+void deleteList(List *list)
+{
+  while (list != nullptr)
+  {
+    List *newList = list->before;
+    delete list;
+    list = newList;
+  }
+}
 
 List* convertArrayToList(int* array, int countElementOfArray)
 {
-  List* head = new List; 
+  List* head = new List;
   head->value = array[0];
   head->next = nullptr;
   head->before = nullptr;
+
   List* nowHead = head;
 
   for (size_t i = 1; i < countElementOfArray; ++i)
   {
-    List* newNewHead = new List;
+    List* newNewHead = nullptr;
+    try
+    {
+      newNewHead = new List;
+    }
+    catch (...)
+    {
+      deleteList(newNewHead);
+    }
     newNewHead->value = array[i];
     newNewHead->next = nullptr;
     newNewHead->before = nowHead;
-
     nowHead->next = newNewHead;
     nowHead = newNewHead;
   }
-  return head;
+  return nowHead;
 }
 
 int main()
 {
   int maxCount = 10;
-  int * array = new int[maxCount];
+  int * array = nullptr;
+  try
+  {
+    array = new int[10];
+  }
+  catch (...)
+  {
+    return 1;
+  }
+
   int input;
   int realCount = 0;
   for (size_t i = 0; i < maxCount; i++)
   {
-    std::cin >> input;
-    if (std::cin.fail() && !std::cin.eof())
-    {
+  	std::cin >> input;
+  	if (std::cin.fail() && !std::cin.eof())
+  	{
       break;
-    }
-    array[i] = input;
-    realCount++;
+  	}
+  	array[i] = input;
+  	realCount++;
   }
 
-  List * arr = convertArrayToList(array, realCount);
+  List* newCoolList = nullptr;
+  try
+  {
+    newCoolList = convertArrayToList(array, realCount);
+  }
+  catch (...)
+  {
+    delete[] array;
+  }
 
   for (size_t i = 0; i < realCount; i++)
   {
-  // круто вывести 
+    std::cout << newCoolList->value << " ";
+    newCoolList = newCoolList->before;
   }
-  // круто очистить память 
+  std::cout << "\n";
+  deleteList(newCoolList);
+  delete[] array;
+  return 0;
 }
