6cc8c32d2d4b005ed2f6e8801ed3829fdcabbc28
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-03 23:31:38 +0300

    funcrions added

--- lanovenko.ivan/K1/main.cpp
+++ lanovenko.ivan/K1/main.cpp
@@ -1,3 +1,39 @@
+#include <iostream>
+#include "bilist.hpp"
+
+
+BiList* toDoubleLinkedList(int* array, size_t res)
+{
+  BiList* head = new BiList{ array[0],nullptr, nullptr };
+  for (size_t i = 1; i < res; i++)
+  {
+    head->next = new BiList{ array[i], head, nullptr };
+    head = head->next;
+  }
+  return head;
+}
+
 int main()
 {
+  size_t res = 0;
+  int* array = new int[10000];
+  int a = 0;
+  while (std::cin >> a && !std::cin.eof())
+  {
+    array[res++] = a;
+    if (res >= 10)
+    {
+      break;
+    }
+  }
+  BiList* buf = toDoubleLinkedList(array, res);
+  int b = 0;
+  for (size_t i = 0; i < res; i++)
+  {
+    b = buf->value;
+    buf = buf->prev;
+    std::cout << b << ' ';
+  }
+  delete[] array;
 }
+
