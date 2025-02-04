6df04988679ff32b923a9967a2b0565427beb51a
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-04 20:57:46 +0300

    valgrind error fixed

--- lanovenko.ivan/K1/main.cpp
+++ lanovenko.ivan/K1/main.cpp
@@ -3,50 +3,50 @@
 
 BiList* toDoubleLinkedList(int* array, size_t res)
 {
-	BiList* head = new BiList{ array[0],nullptr, nullptr };
-	BiList* tail = head;
-	BiList* temporary = head;
-	for (size_t i = 1; i < res; i++)
-	{
-		temporary->next = new BiList{ array[i], temporary, nullptr };
-		tail = temporary->next;
-		temporary = temporary->next;
-	}
-	return tail;
+  BiList* head = new BiList{ array[0],nullptr, nullptr };
+  BiList* tail = head;
+  BiList* temporary = head;
+  for (size_t i = 1; i < res; i++)
+  {
+    temporary->next = new BiList{ array[i], temporary, nullptr };
+    tail = temporary->next;
+    temporary = temporary->next;
+  }
+  return tail;
 }
 
 void deleteList(BiList* tail)
 {
-	while (tail != nullptr)
-	{
-		BiList* prev = tail->prev;
-		delete tail;
-		tail = prev;
-	}
+  while (tail != nullptr)
+  {
+    BiList* prev = tail->prev;
+    delete tail;
+    tail = prev;
+  }
 }
 
 int main()
 {
-	size_t res = 0;
-	int* array = new int[10000];
-	int a = 0;
-	while (std::cin >> a && !std::cin.eof())
-	{
-		array[res++] = a;
-		if (res >= 10)
-		{
-			break;
-		}
-	}
-	BiList* buf = toDoubleLinkedList(array, res);
-	BiList* temporary = buf;
-	int b = 0;
-	for (size_t i = 0; i < res; i++)
-	{
-		int b = temporary->value;
-		std::cout << b << ' ';
-		temporary = temporary->prev;
-	}
-	delete[] array;
-	deleteList(buf);
+  size_t res = 0;
+  int* array = new int[10000];
+  int a = 0;
+  while (std::cin >> a && !std::cin.eof())
+  {
+    array[res++] = a;
+    if (res >= 10)
+    {
+      break;
+    }
+  }
+  BiList* buf = toDoubleLinkedList(array, res);
+  BiList* temporary = buf;
+  int b = 0;
+  for (size_t i = 0; i < res; i++)
+  {
+    int b = temporary->value;
+    std::cout << b << ' ';
+    temporary = temporary->prev;
+  }
+  delete[] array;
+  deleteList(buf);
 }
