30f9d5796380091d35df4fe7b30433d1883ffd47
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-02-04 18:43:17 +0300

    function to delete added

--- lanovenko.ivan/K1/main.cpp
+++ lanovenko.ivan/K1/main.cpp
@@ -5,18 +5,32 @@
 BiList* toDoubleLinkedList(int* array, size_t res)
 {
   BiList* head = new BiList{ array[0],nullptr, nullptr };
+  BiList* tail = head;
+  BiList* temporary = head;
   for (size_t i = 1; i < res; i++)
   {
-    head->next = new BiList{ array[i], head, nullptr };
-    head = head->next;
+    temporary->next = new BiList{ array[i], temporary, nullptr };
+    temporary = temporary->next;
+    tail = temporary;
   }
-  return head;
+  return tail;
+}
+
+void toDeleteList(BiList* head)
+{
+  BiList* current = head;
+  while(current != nullptr)
+  {
+    BiList* next = current->next;
+    delete current;
+    current = next;
+  }
 }
 
 int main()
 {
   size_t res = 0;
-  int* array = new int[10000];
+  int* array = new int[10];
   int a = 0;
   while (std::cin >> a && !std::cin.eof())
   {
@@ -27,13 +41,15 @@ int main()
     }
   }
   BiList* buf = toDoubleLinkedList(array, res);
+  BiList* temporary = buf;
   int b = 0;
   for (size_t i = 0; i < res; i++)
   {
-    b = buf->value;
-    buf = buf->prev;
+    b = temporary->value;
+    temporary = temporary->prev;
     std::cout << b << ' ';
   }
   delete[] array;
+  toDeleteList(buf);
 }
 
