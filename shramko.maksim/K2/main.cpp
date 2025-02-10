a93fd2c4da6b2c5eef9ef857a5f4d5329a3c2508
Maksim_Shramko <maxramko@gmail.com>
2025-02-10 11:55:36 +0300

    Duplicates input start

--- shramko.maksim/K2/main.cpp
+++ shramko.maksim/K2/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+
+struct FwdList {
+  int value;
+  FwdList * next;
+};
+
+FwdList* inDuplicates(FwdList* head, int pos, int c)
+{
+  FwdList* current = head;
+  FwdList* prev = nullptr;
+  for (int i = 1, i < pos, i++)
+  {
+    if (current == nullptr)
+    {
+      break;
+    }
+    prev = current;
+    current = currrent->next;
+  }
+}
+
 int main()
 {}
