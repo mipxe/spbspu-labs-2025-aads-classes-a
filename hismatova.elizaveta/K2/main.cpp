a7146946668337b0af8e919c0394a27e1e2423ea
lee <likachich12@gmail.com>
2025-02-10 12:45:54 +0300

    main function

--- hismatova.elizaveta/K2/main.cpp
+++ hismatova.elizaveta/K2/main.cpp
@@ -43,4 +43,50 @@ int main()
 {
   FwdList* head = nullptr;
   FwdList* tail = nullptr;
+  for (int i = 0; i < 10; ++i)
+  {
+    FwdList* newNode = new FwdList{i, nullptr};
+    if (head == nullptr)
+    {
+      head = newNode;
+      tail = newNode;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+  }
+  int num1 = 0;
+  int num2 = 0;
+  while (std::cin >> num1)
+  {
+    if (!(std::cin >> num2))
+    {
+      break;
+    }
+    try
+    {
+      head = addDubli(head, num1, num2);
+    }
+    catch (const std::out_of_range& e)
+    {
+      std::cerr << e.what() << "\n";
+      deleteList(head);
+      return 1;
+    }
+  }
+  FwdList* current = head;
+  while (current)
+  {
+    std::cout << current->value;
+    current = current->next;
+    if (current)
+    {
+      std::cout << " ";
+    }
+  }
+  std::cout << "\n";
+  deleteList(head);
+  return 0;
 }
