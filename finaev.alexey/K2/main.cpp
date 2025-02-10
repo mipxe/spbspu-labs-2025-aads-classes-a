5ca9d5f3b4435469c9e068c3ed092bf54a21e258
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-02-10 12:12:28 +0300

    pupupu

--- finaev.alexey/K2/main.cpp
+++ finaev.alexey/K2/main.cpp
@@ -1,2 +1,66 @@
+#include <iostream>
+
+struct FwdList {
+  int value;
+  FwdList* next;
+};
+
+FwdList* addNewEls(FwdList* head, size_t i, size_t count)
+{
+  FwdList* tail = head;
+  for (size_t j = 0;(tail != nullptr) && (j < i); ++j)
+  {
+    tail = tail->next;
+  }
+  for (size_t i = 0; i < count; ++i)
+  {
+    FwdList* new_el = new FwdList{ tail->value, tail->next };
+    tail->next = new_el;
+    tail = duplicate;
+  }
+  return tail;
+}
+
+void deleteList(FwdList* head)
+{
+    while (head->next != nullptr)
+    {
+        FwdList* subHead = head->next;
+        delete head;
+        head = subHead;
+    }
+}
+
 int main()
-{}
+{
+  FwdList* head = new FwdList{ 0, nullptr };
+  FwdList* tail = head;
+  for (int i = 1; i < 10; ++i)
+  {
+      FwdList* new_element = nullptr;
+      try
+      {
+          new_element = new FwdList{ i, nullptr };
+      }
+      catch (const std::bad_alloc&)
+      {
+          deleteList(head);
+          throw;
+      }
+      tail->next = new_element;
+      tail = new_element;
+  }
+  ListIterator head_ptr;
+  head_ptr.node = head;
+  int a = 0;
+  int b = 0;
+  while (!std::cin.eof())
+  {
+      std::cin >> a;
+      std::cin >> b;
+      if (std::cin.fail())
+      {
+          break;
+      }
+  }
+}
