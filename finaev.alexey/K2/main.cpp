53a3b9922f6adfa474329dba5c8e08088ebcbd42
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-02-10 12:15:32 +0300

    fix

--- finaev.alexey/K2/main.cpp
+++ finaev.alexey/K2/main.cpp
@@ -12,23 +12,23 @@ FwdList* addNewEls(FwdList* head, size_t i, size_t count)
   {
     tail = tail->next;
   }
-  for (size_t i = 0; i < count; ++i)
+  for (size_t j = 0; j < count; ++j)
   {
     FwdList* new_el = new FwdList{ tail->value, tail->next };
     tail->next = new_el;
-    tail = duplicate;
+    tail = new_el;
   }
   return tail;
 }
 
 void deleteList(FwdList* head)
 {
-    while (head->next != nullptr)
-    {
-        FwdList* subHead = head->next;
-        delete head;
-        head = subHead;
-    }
+  while (head->next != nullptr)
+  {
+    FwdList* subHead = head->next;
+    delete head;
+    head = subHead;
+  }
 }
 
 int main()
@@ -37,18 +37,18 @@ int main()
   FwdList* tail = head;
   for (int i = 1; i < 10; ++i)
   {
-      FwdList* new_element = nullptr;
-      try
-      {
-          new_element = new FwdList{ i, nullptr };
-      }
-      catch (const std::bad_alloc&)
-      {
-          deleteList(head);
-          throw;
-      }
-      tail->next = new_element;
-      tail = new_element;
+    FwdList* new_element = nullptr;
+    try
+    {
+      new_element = new FwdList{ i, nullptr };
+    }
+    catch (const std::bad_alloc&)
+    {
+      deleteList(head);
+      throw;
+    }
+    tail->next = new_element;
+    tail = new_element;
   }
   ListIterator head_ptr;
   head_ptr.node = head;
@@ -56,11 +56,11 @@ int main()
   int b = 0;
   while (!std::cin.eof())
   {
-      std::cin >> a;
-      std::cin >> b;
-      if (std::cin.fail())
-      {
-          break;
-      }
+    std::cin >> a;
+    std::cin >> b;
+    if (std::cin.fail())
+    {
+      break;
+    }
   }
 }
