7a8e9fa5656c4c106a7b5771222343d87ac67360
guseynov.guseyn <ganx.gamer@gmail.com>
2025-02-10 12:04:15 +0300

    nedodelka

--- guseynov.guseyn/K2/main.cpp
+++ guseynov.guseyn/K2/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+struct FwdList
+{
+  int value;
+  FwdList * next;
+};
+
 int main()
 {
+  constexpr size = 10;
+  int index = 0;
+  int count = 0;
+  try
+  {
+    FwdList * head = new FwdList{0, nullptr};
+    FwdList * subhead = head;
+    for (size_t i = 1; i < size; i++)
+    {
+      FwdList * node = nullptr;
+      node = new FwdList{i, nullptr};
+      subhead->next = node;;
+      subhead = node;
+    }
+    delete subhead;
+    i = 0;
+  }
+  catch(std::bad_alloc &)
+  {
+    return 1;
+  }
+  while (!std::cin.eof() && std::cin >> number)
+  {
 
+  }
 }
