2eed9fe763f8d236952839d7aa33731a4671de17
mipxe <mariamozegova8726@gmail.com>
2025-02-10 12:04:57 +0300

    create input list

--- mozhegova.maria/K2/main.cpp
+++ mozhegova.maria/K2/main.cpp
@@ -1,2 +1,38 @@
+#include <iostream>
+
+struct FwdList {
+  int value;
+  FwdList * next;
+};
+
+void deleteList(FwdList * head)
+{
+  FwdList * temp = head;
+  while (temp)
+  {
+    FwdList * nextHead = temp->next;
+    delete temp;
+    temp = nextHead;
+  }
+}
+
 int main()
-{}
+{
+  FwdList * head = nullptr;
+  FwdList * current = nullptr;
+  for (int i = 0; i < 10; i++)
+  {
+    FwdList * subhead = nullptr;
+    try
+    {
+      subhead = new FwdList {i, nullptr};
+    }
+    catch (const std::bad_alloc &)
+    {
+      deleteList(head);
+      throw;
+    }
+    current->next = subhead;
+    current = subhead;
+  }
+}
