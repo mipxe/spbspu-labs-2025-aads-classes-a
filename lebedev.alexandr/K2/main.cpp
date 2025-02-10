43575bd0431c892caad4f095cbafa8efb942d0a7
mokkenz1 <lam1600@mail.ru>
2025-02-10 11:58:31 +0300

    Create print function

--- lebedev.alexandr/K2/main.cpp
+++ lebedev.alexandr/K2/main.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
+
+struct FwdList
+{
+  int value;
+  FwdList* next;
+};
+
+void printList(FwdList* head)
+{
+  while (head != nullptr)
+  {
+    std::cout << head->value;
+    head = head->next;
+  }
+}
+
 int main()
 {}
