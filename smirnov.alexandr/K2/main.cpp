7525c0247bbf28586d72bd87e647d7e76bf6fb73
A1exCRE <alsmmm2065@gmail.com>
2025-02-10 21:08:31 +0300

    fix

--- smirnov.alexandr/K2/main.cpp
+++ smirnov.alexandr/K2/main.cpp
@@ -1,24 +1,27 @@
+#include <iostream>
+#include <cstddef>
+
 struct FwdList
 {
   int value;
   FwdList * next;
 };
 
-void deleteList(FwdList * list)
+void deleteFwdList(FwdList * head)
 {
-  while (list != nullptr)
+  while (head != nullptr)
   {
-    FwdList * next = list->next;
-    delete list;
-    list = next;
+    FwdList * next = head->next;
+    delete head;
+    head = next;
   }
 }
 
-FwdList* createList()
+FwdList * createFwdList()
 {
   FwdList * head = new FwdList{0, nullptr};
-  FwdList* current = head;
-  for (size_t i = 1; i < 10; ++i)
+  FwdList * current = head;
+  for (int i = 1; i < 10; ++i)
   {
     FwdList * newNode = nullptr;
     try
@@ -36,16 +39,65 @@ FwdList* createList()
   return head;
 }
 
+FwdList * insertDuplicates(FwdList * head, size_t index, size_t count)
+{
+  FwdList * current = head;
+  for (size_t i = 1; i < index && current != nullptr; ++i)
+  {
+    current = current->next;
+  }
+  if (current == nullptr)
+  {
+    throw std::out_of_range("Position is out of range");
+  }
+  for (size_t i = 0; i < count; ++i)
+  {
+    FwdList* newNode = new FwdList{current->value, current->next};
+    current->next = newNode;
+    current = newNode;
+  }
+  return head;
+}
+
+void printFwdList(const FwdList * head)
+{
+  std::cout << head->value;
+  head = head->next;
+  while (head != nullptr)
+  {
+    std::cout << " " << head->value;
+    head = head->next;
+  }
+  std::cout << "\n";
+}
+
 int main()
 {
-  FwdList * list = nullptr;
+  FwdList * head = nullptr;
   try
   {
-    list = createList();
+    head = createFwdList();
   }
   catch (const std::bad_alloc & e)
   {
+    deleteFwdList(head);
     std::cerr << "Out of memory\n";
     return 1;
   }
+  size_t index = 0;
+  size_t count = 0;
+  while (std::cin >> index >> count && !std::cin.eof())
+  {
+    try
+    {
+      head = insertDuplicates(head, index, count);
+    }
+    catch (const std::out_of_range & e)
+    {
+      deleteFwdList(head);
+      return 1;
+    }
+  }
+  printFwdList(head);
+  deleteFwdList(head);
 }
