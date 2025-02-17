53c531b2757ffafa4e2d5fc87311b683db5de5f9
A1exCRE <alsmmm2065@gmail.com>
2025-02-17 12:10:13 +0300

    reading the number of arrays

--- smirnov.alexandr/K3/main.cpp
+++ smirnov.alexandr/K3/main.cpp
@@ -1,2 +1,24 @@
+template< class T >
+struct List
+{
+  T data;
+  List< T > * next;
+};
+
 int main()
-{}
+{
+  size_t m;
+  std::cin >> m;
+  if (!std::cin)
+  {
+    throw std::runtime_error("Invalid input for number of arrays");
+  }
+  size_t * n = new size_t[m];
+  for (size_t i = 0; i < m; ++i)
+  {
+    std::cin >> n[i];
+    if (!std::cin)
+    {
+      throw std::runtime_error("Invalid input for array size");
+    }
+}
