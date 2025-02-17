3665ba7891cc37d6c6e617b5ddb00b116512ce98
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-02-17 11:58:49 +0300

    K3

--- maslov.aleksey/K3/main.cpp
+++ maslov.aleksey/K3/main.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+#include <cstddef>
+
 int main()
-{}
+{
+  size_t numArray = 0;
+  if (!(std::cin >> numArray))
+  {
+    std::cerr << "error input number of arrays\n";
+    return 1;
+  }
+  int * array = nullptr;
+  try
+  {
+    array = new int[numArray];
+    for (size_t i = 0; i < numArray; ++i)
+    {
+      if (!(std::cin >> array[i]))
+      {
+        std::cerr << "error input\n";
+      }
+    }
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
+}
