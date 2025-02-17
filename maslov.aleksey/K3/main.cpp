8a5f0adcc6ec1fbdf19d4a1bb4c7f66fa79c426f
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-02-17 15:20:31 +0300

    input array

--- maslov.aleksey/K3/main.cpp
+++ maslov.aleksey/K3/main.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
 #include <cstddef>
 
+template< class T >
+struct List
+{
+  T data;
+  List< T > * next;
+};
+template< class T, class C >
+size_t count(const List< List< T > * > * head, C condition)
+{}
+List< List< int > * > * convert(const int * const * d, size_t m, const size_t * n);
+void deleteArray(int ** array, size_t numArray)
+{
+  for (size_t i = 0; i < numArray; ++i)
+  {
+    delete[] array[i];
+  }
+  delete[] array;
+}
+
 int main()
 {
   size_t numArray = 0;
+  size_t numElements = 0;
   if (!(std::cin >> numArray))
   {
     std::cerr << "error input number of arrays\n";
     return 1;
   }
-  int * array = nullptr;
-  try
+  int ** array = new int*[numArray];
+  for (size_t i = 0; i < numArray; i++)
   {
-    array = new int[numArray];
-    for (size_t i = 0; i < numArray; ++i)
+    if (!(std::cin >> numElements))
     {
-      if (!(std::cin >> array[i]))
+      std::cerr << "error input\n";
+      deleteArray(array, i);
+      return 1;
+    }
+    try
+    {
+      array[i] = new int[numElements];
+    }
+    catch (const std::bad_alloc & e)
+    {
+      std::cerr << "memory error\n";
+      deleteArray(array, i);
+      return 1;
+    }
+    for (size_t j = 0; j < numElements; j++)
+    {
+      if (!(std::cin >> array[i][j]))
       {
         std::cerr << "error input\n";
+        deleteArray(array, i);
+        return 1;
       }
     }
   }
-  catch (const std::exception & e)
-  {
-    std::cerr << e.what() << '\n';
-  }
+  deleteArray(array, numArray);
 }
