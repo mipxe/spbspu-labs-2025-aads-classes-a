0fb2feacd785075262179c6734375f2e6a89685d
mipxe <mariamozegova8726@gmail.com>
2025-02-17 13:39:29 +0300

    create conver function and fix input

--- mozhegova.maria/K3/main.cpp
+++ mozhegova.maria/K3/main.cpp
@@ -6,7 +6,52 @@ struct List {
   List< T > * next;
 };
 
-List< List< int > * > * convert(const int * const * d, size_t m, const size_t * n);
+void deleteList(List< List< int > * > * head)
+{
+  List< List< int > * > * listOfLists = head;
+  while (listOfLists)
+  {
+    List< int > * currentList = listOfLists->data;
+    while (currentList)
+    {
+      List< int > * tempNode = currentList;
+      currentList = currentList->next;
+      delete tempNode;
+    }
+    List< List< int > * > * tempList = listOfLists;
+    listOfLists = listOfLists->next;
+    delete tempList;
+  }
+}
+
+List< List< int > * > * convert(const int * const * d, size_t m, const size_t * n)
+{
+  List< List< int > * > * head = new List< List< int > * > ();
+  List< List< int > * > * tail = head;
+  try
+  {
+    for (size_t i = 0; i < m; i++)
+    {
+      List< int > * subhead = new List< int > {d[i][0], nullptr};
+      List< int > * current = subhead;
+      for (size_t j = 1; j < n[i]; j++)
+      {
+        List< int > * newNode = new List< int > {d[i][j], nullptr};
+        current->next = newNode;
+        current = newNode;
+      }
+      tail->data = subhead;
+      tail->next = nullptr;
+      tail = tail->next;
+    }
+  }
+  catch(...)
+  {
+    deleteList(head);
+    throw;
+  }
+  return head;
+}
 
 template< class T, class C >
 size_t count(const List< List< T > * > * head, C condition);
@@ -22,30 +67,34 @@ void deleteArr(int ** arr, size_t count)
 
 int main()
 {
-  size_t n = 0;
-  std::cin >> n;
-  int ** arrays = nullptr;
+  size_t M = 0;
+  std::cin >> M;
+  int ** arr = nullptr;
   size_t i = 0;
+  size_t * n = nullptr;
   try
   {
-    arrays = new int * [n];
-    for (; i < n; i++)
+    arr = new int * [M];
+    n = new size_t[M];
+    for (; i < M; i++)
     {
-      size_t m = 0;
-      std::cin >> m;
-      int * num = new int[m];
-      for (size_t j = 0; j < m; j++)
+      size_t N = 0;
+      std::cin >> N;
+      arr[i] = new int[N];
+      n[i] = N;
+      for (size_t j = 0; j < N; j++)
       {
-        int a = 0;
-        std::cin >> a;
-        num[j] = a;
+        std::cin >> arr[i][j];
       }
     }
   }
   catch(...)
   {
-    deleteArr(arrays, i);
+    delete[] n;
+    deleteArr(arr, i);
     std::cerr << "Out of memory\n";
     return 1;
   }
+
+
 }
