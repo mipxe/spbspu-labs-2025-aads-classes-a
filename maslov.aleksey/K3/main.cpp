c3cd1cc4ab9b6504ac1bb495e4890959f8eaa5e9
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-02-17 17:02:56 +0300

    convert func part 1

--- maslov.aleksey/K3/main.cpp
+++ maslov.aleksey/K3/main.cpp
@@ -7,10 +7,29 @@ struct List
   T data;
   List< T > * next;
 };
-template< class T, class C >
-size_t count(const List< List< T > * > * head, C condition)
-{}
-List< List< int > * > * convert(const int * const * d, size_t m, const size_t * n);
+List< List< int > * > * convert(const int * const * d, size_t m, const size_t * n)
+{
+  List< List< int > * > * head = nullptr;
+  for (size_t i = 0; i < m; ++i)
+  {
+    List< int > * inHead = nullptr;
+    List< int > * inTail = nullptr;
+    try
+    {
+      inHead = new List< int >{d[i][0], nullptr};
+      inTail = inHead;
+      for (size_t j = 1; j < n[i]; ++j)
+      {
+        List< int > * newElement = new List< int >{d[i][j], nullptr};
+        inTail->next = newElement;
+        inTail = newElement;
+      }
+    }
+    catch (const std::bad_alloc &)
+    {}
+  }
+  return head;
+}
 void deleteArray(int ** array, size_t numArray)
 {
   for (size_t i = 0; i < numArray; ++i)
@@ -23,37 +42,40 @@ void deleteArray(int ** array, size_t numArray)
 int main()
 {
   size_t numArray = 0;
-  size_t numElements = 0;
   if (!(std::cin >> numArray))
   {
     std::cerr << "error input number of arrays\n";
     return 1;
   }
   int ** array = new int*[numArray];
+  size_t * numElements = new size_t[numArray];
   for (size_t i = 0; i < numArray; i++)
   {
-    if (!(std::cin >> numElements))
+    if (!(std::cin >> numElements[i]))
     {
       std::cerr << "error input\n";
       deleteArray(array, i);
+      delete[] numElements;
       return 1;
     }
     try
     {
-      array[i] = new int[numElements];
+      array[i] = new int[numElements[i]];
     }
     catch (const std::bad_alloc & e)
     {
       std::cerr << "memory error\n";
       deleteArray(array, i);
+      delete[] numElements;
       return 1;
     }
-    for (size_t j = 0; j < numElements; j++)
+    for (size_t j = 0; j < numElements[i]; j++)
     {
       if (!(std::cin >> array[i][j]))
       {
         std::cerr << "error input\n";
         deleteArray(array, i);
+        delete[] numElements;
         return 1;
       }
     }
