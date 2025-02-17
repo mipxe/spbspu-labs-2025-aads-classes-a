2bbcab93e9060ed4e50287dc6dcd5da496fb70f9
Nekich06 <p.nikita.73526@gmail.com>
2025-02-17 14:22:18 +0300

    Implemented input

--- petrov.nikita/K3/main.cpp
+++ petrov.nikita/K3/main.cpp
@@ -7,25 +7,53 @@ struct List
   List< T > * next;
 };
 
-int * allocateMemoryForMassive(int * ptr_massives, size_t number_of_elements);
-void clearAllMassives(int ** ptr_massives, size_t number_of_massives);
+void allocateMemoryForMassive(int * ptr_massives, size_t number_of_elements);
+void clearAllMassives(int ** ptr_massives, size_t * ptr_numbers_of_elements, size_t number_of_massives);
 List< List< int > * > * convert(const int * const * d, size_t m, const size_t * n);
 
 int main()
 {
   int ** ptr_massives = nullptr;
   size_t number_of_massives = 0;
-  size_t number_of_elements = 0;
-  std::cin >> number_of_massives;
-  ptr_massives = new int * [number_of_massives];
-  for (size_t i = 0; i < number_of_massives; i++)
+  if (!(std::cin >> number_of_massives))
   {
-    std::cin >> number_of_elements;
-    allocateMemoryForMassive(ptr_massives[i], number_of_elements);
+    std::cerr << "ERROR: Invalid argument" << "\n";
+    return 1;
   }
+  size_t * ptr_numbers_of_elements = nullptr;
+  size_t number_of_elements = 0;
+  size_t created = 0;
+  try
+  {
+    ptr_massives = new int * [number_of_massives];
+    ptr_numbers_of_elements = new size_t [number_of_massives];
+    for (size_t i = 0; i < number_of_massives; i++)
+    {
+      if (!(std::cin >> number_of_elements) || std::cin.eof())
+      {
+        throw std::logic_error("ERROR: Invalid argument");
+      }
+      allocateMemoryForMassive(ptr_massives[i], number_of_elements);
+      created++;
+      ptr_numbers_of_elements[i] = number_of_elements;
+    }
+  }
+  catch (const std::logic_error & e)
+  {
+    clearAllMassives(ptr_massives, ptr_numbers_of_elements, created);
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  catch(const std::bad_alloc & e)
+  {
+    clearAllMassives(ptr_massives, ptr_numbers_of_elements, created);
+    std::cerr << "ERROR: Out of memory" << "\n";
+    return 3;
+  }
+  convert(ptr_massives, number_of_massives, ptr_numbers_of_elements);
 }
 
-int * allocateMemoryForMassive(int * ptr_massive, size_t number_of_elements)
+void allocateMemoryForMassive(int * ptr_massive, size_t number_of_elements)
 {
   ptr_massive = new int [number_of_elements];
   for (size_t i = 0; i < number_of_elements; i++)
@@ -34,7 +62,12 @@ int * allocateMemoryForMassive(int * ptr_massive, size_t number_of_elements)
   }
 }
 
-void clearAllMassives(int ** ptr_massives, size_t number_of_massives)
+void clearAllMassives(int ** ptr_massives, size_t * ptr_numbers_of_elements, size_t created)
 {
-  
+  for (size_t i = 0; i < created; i++)
+  {
+    delete[] ptr_massives[i];
+  }
+  delete[] ptr_massives;
+  delete[] ptr_numbers_of_elements;
 }
