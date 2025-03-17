df5cf9cca58da06abbf836d224408220db9420f5
A1exCRE <alsmmm2065@gmail.com>
2025-03-17 11:52:11 +0300

    create input

--- smirnov.alexandr/K6/main.cpp
+++ smirnov.alexandr/K6/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+#include <cstddef>
+
+template< class T >
+struct BiTree {
+  T data;
+  BiTree< T > * left, * right, * parent;
+};
+
 int main()
-{}
+{
+  size_t length = 0;
+  std::cin >> length;
+  int * arr = new int[length];
+  for (size_t i = 0; i < length; ++i)
+  {
+    if (!(std::cin >> arr[i]))
+    {
+      delete[] arr;
+      std::cerr << "Input error\n";
+      return 1;
+    }
+  }
+}
