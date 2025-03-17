6b555dd815675fd4158a06b73ed83e3be2857173
mokkenz1 <lam1600@mail.ru>
2025-03-17 11:15:36 +0300

    Create array input

--- lebedev.alexandr/K6/main.cpp
+++ lebedev.alexandr/K6/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+
+template< class T >
+struct BiTree
+{
+  T data;
+  BiTree *left, *right, *parent;
+};
+
 int main()
-{}
+{
+  size_t size = 0;
+  std::cin >> size;
+  int* arr = new int[size];
+  for (size_t i = 0; i < size; i++)
+  {
+    if (!(std::cin >> arr[i]))
+    {
+      delete[] arr;
+      std::cerr << "Incorrect input!\n";
+      return 1;
+    }
+  }
+}
