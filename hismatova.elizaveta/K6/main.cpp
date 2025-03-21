a4c6ed5128d3bc02d84c36d6ff9e27bac12823a1
lee <likachich12@gmail.com>
2025-03-21 20:32:55 +0300

    k6

--- hismatova.elizaveta/K6/main.cpp
+++ hismatova.elizaveta/K6/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 template< class T >
 struct BiTree
 {
@@ -20,16 +23,16 @@ BiTree< T >* rotate_right(BiTree< T >* root)
   {
     return nullptr;
   }
-  BiTree< T >* new_root = root->left;
-  root->left = new_root->right;
-  if (new_root->right)
+  BiTree< T >* newRoot = root->left;
+  root->left = newRoot->right;
+  if (newRoot->right)
   {
-    new_root->right->parent = root;
+    newRoot->right->parent = root;
   }
-  new_root->parent = root->parent;
-  new_root->right = root;
-  root->parent = new_root;
-  return new_root;
+  newRoot->parent = root->parent;
+  newRoot->right = root;
+  root->parent = newRoot;
+  return newRoot;
 }
 template< class T >
 BiTree< T >* rotate_left(BiTree< T >* root)
@@ -38,16 +41,16 @@ BiTree< T >* rotate_left(BiTree< T >* root)
   {
     return nullptr;
   }
-  BiTree< T >* new_root = root->right;
-  root->right = new_root->left;
-  if (new_root->left)
+  BiTree< T >* newRoot = root->right;
+  root->right = newRoot->left;
+  if (newRoot->left)
   {
-    new_root->left->parent = root;
+    newRoot->left->parent = root;
   }
-  new_root->parent = root->parent;
-  new_root->left = root;
-  root->parent = new_root;
-  return new_root;
+  newRoot->parent = root->parent;
+  newRoot->left = root;
+  root->parent = newRoot;
+  return newRoot;
 }
 template< class T, class Cmp >
 BiTree< T >* find(BiTree< T >* root, const T& value, Cmp cmp)
@@ -95,9 +98,99 @@ void insert(BiTree< int >*& root, int value)
   else
   {
     insert(root->right, value);
-    if (root->right) root->right->parent = root;
+    if (root->right)
+    {
+      root->right->parent = root;
+    }
   }
 }
-
 int main()
-{}
+{
+  int length = 0;
+  std::cin >> length;
+  if (std::cin.fail() || length < 0)
+  {
+    std::cerr << "ERROR: invalid input\n";
+    return 1;
+  }
+  BiTree< int >* root = nullptr;
+  for (int i = 0; i < length; ++i)
+  {
+    int value = 0;
+    std::cin >> value;
+    if (std::cin.fail())
+    {
+      std::cerr << "ERROR: invalid input\n";
+      return 1;
+    }
+    try
+    {
+      insert(root, value);
+    }
+    catch (const std::bad_alloc&)
+    {
+      std::cerr << "ERROR: out of memory\n";
+      return 1;
+    }
+  }
+  std::string command;
+  while (std::getline(std::cin, command))
+  {
+    if (command.empty())
+    {
+      continue;
+    }
+    std::istringstream is(command);
+    std::string rotation;
+    int value = 0;
+    if (!(is >> rotation >> value))
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      return 1;
+    }
+    BiTree< int >* node = find(root, value, std::less<int>());
+    if (!node)
+    {
+      std::cout << "<INVALID ROTATE>\n";
+      continue;
+    }
+    if (rotation == "left")
+    {
+      BiTree< int >* newRoot = rotate_left(node);
+      if (!newRoot)
+      {
+        std::cout << "<INVALID ROTATE>\n";
+      }
+      else
+      {
+        std::cout << newRoot->data << "\n";
+        if (newRoot->parent == nullptr)
+        {
+          root = newRoot;
+        }
+      }
+    }
+    else if (rotation == "right")
+    {
+      BiTree< int >* newRoot = rotate_right(node);
+      if (!newRoot)
+      {
+        std::cout << "<INVALID ROTATE>\n";
+      }
+      else
+      {
+        std::cout << newRoot->data << "\n";
+        if (newRoot->parent == nullptr)
+        {
+          root = newRoot;
+        }
+      }
+    }
+    else
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      return 1;
+    }
+  }
+  return 0;
+}
